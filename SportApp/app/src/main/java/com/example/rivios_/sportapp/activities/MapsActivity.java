2741e5dc67011d151339a3d70e34b3d37a587ff8
mivosevic <mivos6@gmail.com>
2017-09-14 20:38:34 +0200

    Crtanje rute pomoću markera

--- SportApp/app/src/main/java/com/example/rivios_/sportapp/activities/MapsActivity.java
+++ SportApp/app/src/main/java/com/example/rivios_/sportapp/activities/MapsActivity.java
@@ -3,7 +3,7 @@ package com.example.rivios_.sportapp.activities;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
-
+import android.view.View;
 
 import com.example.rivios_.sportapp.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -19,15 +19,18 @@ import com.google.maps.model.DirectionsResult;
 import com.google.maps.model.TravelMode;
 import com.google.maps.model.LatLng;
 
-import org.joda.time.DateTime;
-
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    private GoogleMap mMap;
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+    private GoogleMap map;
     private GeoApiContext geoCtx;
+
+    private static final LatLng cro = new LatLng(44.7380161, 18.1921251);
+
+    ArrayList<LatLng> checkpoints = new ArrayList<LatLng>();
+    String encodedRoute = "";
+    long routeLength = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,15 +54,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        LatLng bezveze = new LatLng(-34, 149);
-        //mMap.addMarker(new MarkerOptions().position(convert(sydney)).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(convert(sydney)));
-        //mMap.addMarker(new MarkerOptions().position(convert(bezveze)).title("Marker in Bezveze"));
-
+        map = googleMap;
         geoCtx = new GeoApiContext.Builder()
                 .apiKey(getString(R.string.google_directions_key))
                 .queryRateLimit(3)
@@ -68,40 +63,61 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .writeTimeout(1, TimeUnit.SECONDS)
                 .build();
 
-        DateTime now = new DateTime();
-
-        DirectionsResult results;
-        try {
-            results = DirectionsApi.newRequest(geoCtx)
-                    .mode(TravelMode.DRIVING)
-                    .origin(sydney)
-                    .destination(bezveze)
-                    .departureTime(now)
-                    .await();
-        }
-        catch (Exception e)
-        {
-            Log.d("PERO", "Nesto ne valja s mapom.");
-            Log.d("PERO", e.toString());
-            return;
-        }
-
-        mMap.addMarker(new MarkerOptions().position(convert(new LatLng(results.routes[0].legs[0].startLocation.lat,results.routes[0].legs[0].startLocation.lng))).title("start"));
-        mMap.addMarker(new MarkerOptions().position(convert(new LatLng(results.routes[0].legs[0].endLocation.lat,results.routes[0].legs[0].endLocation.lng))).title("end"));
-
-        ArrayList<com.google.android.gms.maps.model.LatLng> decodedPath = new ArrayList<>();
-        for (LatLng ll : results.routes[0].overviewPolyline.decodePath())
-        {
-            decodedPath.add(convert(ll));
-        }
-
-        Log.d("PERO", "Broj tocaka " + decodedPath.size());
-
-        mMap.addPolyline(new PolylineOptions().addAll(decodedPath));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(convert(cro), 8));
+        map.setOnMapClickListener(this);
     }
 
     private com.google.android.gms.maps.model.LatLng convert(LatLng ll)
     {
         return new com.google.android.gms.maps.model.LatLng(ll.lat, ll.lng);
+    }
+
+    @Override
+    public void onMapClick(com.google.android.gms.maps.model.LatLng latLng) {
+        MarkerOptions opt = new MarkerOptions().position(latLng);
+        if (checkpoints.size() == 0) {
+            opt.title("Start");
+        } else {
+            opt.title("Stanica " + checkpoints.size());
+        }
+        map.addMarker(opt);
+        checkpoints.add(new LatLng(latLng.latitude, latLng.longitude));
+
+        DirectionsResult results;
+
+        if (checkpoints.size() > 1) {
+            try {
+                results = DirectionsApi.newRequest(geoCtx)
+                        .mode(TravelMode.WALKING)
+                        .origin(checkpoints.get(checkpoints.size() - 2))
+                        .destination(checkpoints.get(checkpoints.size() - 1))
+                        .await();
+            } catch (Exception e) {
+                Log.d("PERO", "Nesto ne valja s mapom.");
+                Log.d("PERO", e.toString());
+                return;
+            }
+
+            encodedRoute += results.routes[0].overviewPolyline.getEncodedPath();
+            routeLength += results.routes[0].legs[0].distance.inMeters;
+
+            ArrayList<com.google.android.gms.maps.model.LatLng> decodedPath = new ArrayList<>();
+            for (LatLng ll : results.routes[0].overviewPolyline.decodePath())
+            {
+                decodedPath.add(convert(ll));
+            }
+
+            map.addPolyline(new PolylineOptions().addAll(decodedPath));
+        }
+    }
+
+    public void undoMarker(View v)
+    {
+
+    }
+
+    public void saveRoute(View v)
+    {
+
     }
 }
